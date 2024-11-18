057f6014fe63d9da1369f1f9100daf08af4bb226
marcosdaniel9117 <146689989+marcosdaniel9117@users.noreply.github.com>
2024-11-18 02:06:03 -0400

    Add files via upload

---
+++ MainDatos.py
@@ -0,0 +1,8 @@
+from Datos import Datos
+Datos = Datos("Marcos", "Janco",  "25", "Estudiante")
+
+Datos.mostrar()
+print(Datos.Nombre) 
+print(Datos.Apellido) 
+print(Datos.Edad)   
+print(Datos.Profesion()) 
