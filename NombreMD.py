ead8f7f3e72f9960832cdc082cd6d41306c2de39
marcosdaniel9117 <146689989+marcosdaniel9117@users.noreply.github.com>
2024-11-18 01:22:36 -0400

    Add files via upload

---
+++ NombreMD.py
@@ -0,0 +1,8 @@
+class Persona:
+    def __init__(self, n, a):
+        self.MarcosDaniel= n
+        self.Janco = a
+    
+    def mostrar(self):
+        print(f"Nombres: {self.MarcosDaniel}")
+        print(f"Apellidos: {self.Janco}")
