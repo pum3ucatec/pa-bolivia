af379cca62d243a7a43b598e6d5bc097e288885d
marcosdaniel9117 <146689989+marcosdaniel9117@users.noreply.github.com>
2024-11-18 02:05:42 -0400

    Delete NombreMD.py

--- NombreMD.py
+++
@@ -1,8 +0,0 @@
-class Persona:
-    def __init__(self, n, a):
-        self.MarcosDaniel= n
-        self.Janco = a
-    
-    def mostrar(self):
-        print(f"Nombres: {self.MarcosDaniel}")
-        print(f"Apellidos: {self.Janco}")
