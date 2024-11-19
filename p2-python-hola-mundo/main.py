5c5baec635331fdb8b1e18c3d0bcea3566a35d7e
RaulitoHeredia <super20002h@gmail.com>
2024-11-19 14:57:13 -0400

    Practica 2 Raul Heredia Clase Bicicleta

--- p2-python-hola-mundo/main.py
+++ p2-python-hola-mundo/main.py
@@ -1,5 +1,6 @@
 from persona import Persona
 from docente import Docente
+from bicicleta import Bicicleta
 
 objGaston = Persona("Gaston Genaro", "Quelali Calcina")
 objRaul = Persona("Raul", "Heredia")
@@ -10,3 +11,5 @@ objRaul.mostrar()
 objGastonDocente = Docente("GASTON GENARO", "QUELALI CALCINA", "Lic. en Ing. de Sistemas", 451245)
 objGastonDocente.mostrar()
 
+objBici = Bicicleta("BMX", "Montañera", "Adulto","Negro")
+objBici.mostrar()
