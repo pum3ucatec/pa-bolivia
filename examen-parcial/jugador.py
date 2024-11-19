285614b5cafd28d1dcd54a7fcb060bc73e767ad1
CarMichel <carlos.michel.ha@gmail.com>
2024-11-19 19:51:51 -0400

    anadir examen parcial

---
+++ examen-parcial/jugador.py
@@ -0,0 +1,11 @@
+from jugador import Jugador
+
+# Crear tres objetos de la clase Jugador
+jugador1 = Jugador("Lionel Messi", "Delantero", 36)
+jugador2 = Jugador("Cristiano Ronaldo", "Delantero", 39)
+jugador3 = Jugador("Neymar Jr.", "Delantero", 32)
+
+# Mostrar la información de cada jugador
+jugador1.mostrar()
+jugador2.mostrar()
+jugador3.mostrar()
