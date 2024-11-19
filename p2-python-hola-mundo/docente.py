3d6b20b317e4df0a6fb935639497ad8a64c7efc5
MAG <mauritrabajosucatec@gmail.com>
2024-11-19 14:49:34 -0400

    modificacion de Docente a Celular

--- p2-python-hola-mundo/docente.py
+++ p2-python-hola-mundo/docente.py
@@ -1,13 +1,14 @@
-class Docente:
-    def __init__(self, nombres, apellidos, profesion, registro):
-        self.nombres = nombres
-        self.apellidos = apellidos
-        self.profesion = profesion
-        self.registro = registro
+class Celular:
+    def __init__(self, marca, modelo, caracteristica, codigo):
+        self.marcas = marcas
+        self.modelos = modelos
+        self.caracteristica = caracteristica
+        self.codigo = codigo
     
     def mostrar(self):
         print(f"+++++++++++++++++++++++")
-        print(f"Registro: {self.registro}")
-        print(f"Nombres: {self.nombres}")
-        print(f"Apellidos: {self.apellidos}")
-        print(f"Profesion: {self.profesion}")
+        print(f"Codigo: {self.codigo}")
+        print(f"Marca: {self.marcas}")
+        print(f"Modelos: {self.modelos}")
+        print(f"Caracteristica: {self.caracteristica}")
+    
--- p2-python-hola-mundo/main.py
+++ p2-python-hola-mundo/main.py
@@ -1,12 +1,10 @@
 from persona import Persona
-from docente import Docente
 
-objGaston = Persona("Gaston Genaro", "Quelali Calcina")
-objRaul = Persona("Raul", "Heredia")
+objXiomi = Persona("Xiomi Redmi", "10 note Pro")
+objSamsung = Persona("Samsung", "Galaxy S5")
 
-objGaston.mostrar()
-objRaul.mostrar()
+objXiomi.mostrar()
+objSamsung.mostrar()
 
-objGastonDocente = Docente("GASTON GENARO", "QUELALI CALCINA", "Lic. en Ing. de Sistemas", 451245)
-objGastonDocente.mostrar()
-
+objXiaomiDocente = Persona("XIOMI REDMI", "10 NOTE PRO", "gama media", 555657585960)
+objXiaomiDocente.mostrar()
