10d44172eadfcc596bc087f9070f12971155c4a9
miuBey <beyloreprado@gmail.com>
2024-11-19 20:27:56 -0400

    realizando nuevos cambios

--- .vscode/main.py/main.py
+++ .vscode/main.py/main.py
@@ -3,8 +3,8 @@ from jugador import Jugador  # Importamos la clase Jugador desde su archivo
 def main():
     # Crear tres objetos de la clase Jugador
     jugador1 = Jugador("Mauricio", "Delantero", 36)
-    jugador2 = Jugador("Fernando", "Delantero", 39)
-    jugador3 = Jugador("Raul", "Delantero", 24)
+    jugador2 = Jugador("Fernando", "Medio", 39)
+    jugador3 = Jugador("Raul", "Arquero", 24)
 
     # Mostrar la información de los jugadores
     print("Información de los jugadores:")
