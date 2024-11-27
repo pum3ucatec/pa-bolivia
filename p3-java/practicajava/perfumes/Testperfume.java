15642c759dff764c5133a1ae2bfa2903cd47cba6
CarMichel <carlos.michel.ha@gmail.com>
2024-11-27 19:57:07 -0400

    Subir practica 3 Carlos Michel

---
+++ p3-java/practicajava/perfumes/Testperfume.java
@@ -0,0 +1,13 @@
+package perfumes;
+
+public class Testperfume {
+    public static void main(String[] args) {
+        Perfume perfume1 = new Perfume("Jean Paul Gaultier", "Scandal");
+        Perfume perfume2 = new Perfume("Valentino", "Born in Roma");
+        Perfume perfume3 = new Perfume("Lattafa", "Rave");
+        
+        perfume1.Mostrar();
+        perfume2.Mostrar();
+        perfume3.Mostrar();
+     }
+}
---
+++ p3-java/practicajava/suma.java
@@ -0,0 +1,12 @@
+public class suma {
+    public static void main (String[] args) {
+        int numero1; //declaracion de la variable numero1 de tipo entero
+        int numero2; //declaracion de la variable numero2 de tipo entero
+        int suma;
+        numero1=25; //asignamos el valor de 5 a la variable numero1
+        numero2=13; //asignamos el valor de 8 a la variable numero2
+        suma=numero1+numero2;
+        System.out.println("la suma de " + numero1 + " y " + numero2 + " es:" +suma);
+        
+    }
+}
