a7702152051b3cbcd3f19a8889e0dd04ecfc88d4
edwin quisbert montalvo <e.quisbert@umss.edu.bo>
2024-12-03 19:54:07 -0400

    Practica 4 Circulo y Cuadrado

--- p4-geometria/geometria/Principal.java
+++ p4-geometria/geometria/Principal.java
@@ -7,15 +7,21 @@ import java.awt.*;
 public class Principal extends JPanel {
 
     public Rectangulo r1;
+    public Circulo c1;
+    public Cuadrado s1;
 
     public Principal() {
         this.r1 = new Rectangulo(new Point(300, 200), new Point(500, 300), Color.RED);
+        this.c1 = new Circulo(new Point(200, 200), 50, Color.BLUE);
+        this.s1 = new Cuadrado(new Point(100, 100), 100, Color.GREEN);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.r1.dibujar(g);
+        this.c1.dibujar(g);
+        this.s1.dibujar(g);
     }
 
     public static void main(String[] args) {
