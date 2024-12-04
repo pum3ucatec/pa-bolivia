6b25298bffea8eb9044900e3e926ec0b20ddeeee
pum3ucatec <pum3@ucatec.edu.bo>
2024-12-04 19:45:40 -0400

    Add class Cuadrado con herencia (#10)

--- p4-geometria/geometria/Cuadrado.java
+++ p4-geometria/geometria/Cuadrado.java
@@ -2,30 +2,11 @@ package geometria;
 
 import java.awt.Point;
 import java.awt.Color;
-import java.awt.Graphics;
 
 public class Cuadrado extends Rectangulo {
+    public int lado;
 
-    public Cuadrado(Point i, int lado, Color c) {
-        super(i, new Point(i.x + lado, i.y + lado), c);
-    }
-
-    @Override
-    public double calcularArea() {
-        int lado = Math.abs(this.inicial.x - this.fin.x);
-        return lado * lado;
-    }
-
-    @Override
-    public double calcularPerimetro() {
-        int lado = Math.abs(this.inicial.x - this.fin.x);
-        return 4 * lado;
-    }
-
-    @Override
-    public void dibujar(Graphics g) {
-        g.setColor(this.color);
-        int lado = Math.abs(this.inicial.x - this.fin.x);
-        g.fillRect(this.inicial.x, this.inicial.y, lado, lado);
+    public Cuadrado(Point i, int l, Color c) {
+        super(i, new Point(i.x + l, i.y + l), c);
     }
 }
